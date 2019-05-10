aebf15cb02378297b307c276cf44fd3135175459
Cristian <cefunck@miuandes.cl>
2019-05-09 23:07:51 -0400

    added DIV display, button keyboard visibility, console log

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -2,3 +2,28 @@
 $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
 });
+
+$(document).ready(function(){
+
+	$(".key").click(function(){
+		var text = $(".textarea").text();
+		$('.textarea').text(text+=this.textContent);
+		console.log(this.textContent);
+	});
+
+	$("button").click(function(){
+		if (this.textContent == "hide keyboard"){
+			this.textContent = "show keyboard";
+			$("#kcontainer").hide();
+		}
+		else {
+			this.textContent = "hide keyboard";
+			$("#kcontainer").show();
+		}
+	});
+});
+
+
+function keyboardController(id){
+	this.target = id;
+}
